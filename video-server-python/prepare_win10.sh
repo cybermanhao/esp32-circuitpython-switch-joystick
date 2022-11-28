5929e291cde14192e16c4f2199db387f8d42000a
hao <324677869@qq.com>
2022-11-29 01:33:23 +0800

    windows prepare

---
+++ video-server-python/prepare_win10.sh
@@ -0,0 +1,8 @@
+python -m venv env
+
+./env/Scripts/activate
+
+pip install opencv-python PySide6 ffmpeg-python pyinstaller auto-py-to-exe pillow --index-url http://mirrors.aliyun.com/pypi/simple/ --trusted-host mirrors.aliyun.com
+
+
+auto-py-to-exe
